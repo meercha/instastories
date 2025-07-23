5a916a3ec260b164c6f3df001a20cdf0d4646d83
Mircea Ghenciu <miirrcea@gmail.com>
2025-07-23 10:27:37 +0300

    Add a new endpoint with more unique data

--- InstaStories/Networking/Api.swift
+++ InstaStories/Networking/Api.swift
@@ -10,6 +10,6 @@ import Foundation
 struct Api {
   
   struct Stories {
-    static let endpoint: String = "https://gist.githubusercontent.com/meercha/07c841a5ee6a7263343d3394b1715990/raw/267dee63cd0aa7bffc290c9ab6d7a8047a493fff/gistfile1.txt"
+    static let endpoint: String = "https://gist.githubusercontent.com/meercha/07c841a5ee6a7263343d3394b1715990/raw/09d44dbc53353aeb95a250a9fd929ad148e35e9d/gistfile1.txt"
   }
 }
